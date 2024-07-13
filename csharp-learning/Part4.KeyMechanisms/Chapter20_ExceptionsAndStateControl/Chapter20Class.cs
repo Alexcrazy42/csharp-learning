64563eecea0519573cd58a382a50d6170ba15b95
Alex42 <alexcrazy42@mail.ru>
2024-07-14 01:42:41 +0300

    +

--- csharp-learning/Part4.KeyMechanisms/Chapter20_ExceptionsAndStateControl/Chapter20Class.cs
+++ csharp-learning/Part4.KeyMechanisms/Chapter20_ExceptionsAndStateControl/Chapter20Class.cs
@@ -1,8 +1,56 @@
-﻿namespace csharp_learning.Part4.KeyMechanisms.Chapter20_ExceptionsAndStateControl;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+
+namespace csharp_learning.Part4.KeyMechanisms.Chapter20_ExceptionsAndStateControl;
 
 public class Chapter20Class
 {
     public void Execute()
+    {
+        try
+        {
+            Console.WriteLine("In try");
+        }
+        finally
+        {
+            Type1.M();
+        }
+
+        RuntimeHelpers.PrepareConstrainedRegions();
+
+        try
+        {
+            Console.WriteLine("In try 2");
+        }
+        finally
+        {
+            Type2.M();
+        }
+    }
+}
+
+public sealed class Type1
+{
+    static Type1()
+    {
+        Console.WriteLine("Type1's static ctor called");
+    }
+
+    public static void M()
+    {
+
+    }
+}
+
+public class Type2
+{
+    static Type2()
+    {
+        Console.WriteLine("Type2's static ctor called");
+    }
+
+    [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+    public static void M()
     {
 
     }

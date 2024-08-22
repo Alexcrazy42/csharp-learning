97055814da4bdbf321d4efbd02f1470ea03903dc
Alex42 <alexcrazy42@mail.ru>
2024-08-22 12:15:02 +0300

    end 2 chapter

---
+++ Concurrency/Concurrency/Concurrency/Chapter3.AsyncThreads/Chapter3Class.cs
@@ -0,0 +1,9 @@
+﻿namespace Concurrency.Chapter3.AsyncThreads;
+
+internal class Chapter3Class : IChapter
+{
+    public Task Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Concurrency/Concurrency/Concurrency/Program.cs
+++ Concurrency/Concurrency/Concurrency/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Concurrency;
 using Concurrency.Chapter1.ConcurrencyGeneralInformation;
 using Concurrency.Chapter2.AsyncBasics;
+using Concurrency.Chapter3.AsyncThreads;
 
-IChapter chapter = new Chapter2Class();
+IChapter chapter = new Chapter3Class();
 await chapter.Execute();

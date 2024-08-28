103e8dcf11b7e3b2dc27b5624e714067745de6f1
Alex42 <alexcrazy42@mail.ru>
2024-08-28 12:07:36 +0300

    end 5 chapter

---
+++ Concurrency/Concurrency/Chapter6.SystemReactiveBasics/Chapter6Class.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Concurrency.Chapter6.SystemReactiveBasics;
+
+internal class Chapter6Class : IChapter
+{
+    public async Task Execute()
+    {
+        await Task.Delay(0);
+
+        var progress = new Progress<int>();
+
+        IObservable<EventPattern<int>> progressReports = Observable.FromEventPattern<int>(
+            handler => progress.ProgressChanged += handler,
+            handler => progress.ProgressChanged -= handler);
+
+        progressReports.Subscribe(data => Trace.WriteLine("OnNext: " + data.EventArgs));
+    }
+}
